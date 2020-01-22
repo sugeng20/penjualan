cfacff3bb238d357e0b453090ff3b5a28d71816c
sugeng20 <sugeng.wanantara@gmail.com>
2020-01-22 21:10:55 +0700

    menambahkan upload pertama

---
+++ application/controllers/Admin.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+    public function index()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('admin/admin');
+        $this->load->view('templates/footer');
+    }
+}
